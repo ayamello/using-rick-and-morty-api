80a34c5734ba90cd53b9689d0424893a36ca16b2
Ayana Mello <ayanamello27@gmail.com>
2021-07-07 23:09:10 -0300

    Add pagination

--- src/App.js
+++ src/App.js
@@ -4,17 +4,36 @@ import './App.css';
 
 function App() {
     const [characterList, setCharacterList] = useState([]);
+    const [page, setPage] = useState(1);
+
 
     useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character/')
+        fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
         .then(response => response.json())
         .then(response => setCharacterList(response.results))
         .catch(err => console.log(err));
-    }, []);
+    }, [page]);
+
+    console.log(characterList);
+
+    function nextPage() {
+        setPage(page + 1);
+    }
+
+    function previousPage() {
+        if(page > 1) {
+            setPage(page - 1);
+        }
+    }
 
     return(
         <div className="App">
             <Characters characterList={characterList} />
+            
+            <div className="BtnPages">
+                <button onClick={previousPage}>Previous</button>
+                <button onClick={nextPage}>Next</button>
+            </div>
 
             <footer>
                 <p>Time-space travel</p>
